7fb045331b1cb42ae99a57c19b5428dac560b8c1
zevolpato <jose.avjunior@sp.senac.br>
2020-09-25 13:27:33 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <h1>React - Seja bem vindo. </h1>
+      <h1>React - Seja bem vindo.</h1>
+      <hr />
+      <h2>Em desenvolvimento..</h2>
     </div>
   );
 }
